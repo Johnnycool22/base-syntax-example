03bc47fcb938e9b044a9ced8c76c69df47c2dfd5
John <jkforsythe@hotmail.com>
2020-05-04 11:17:49 +0100

    inline styling and seperate styling CSS file

--- base-syntax-example/src/UserInput/UserInput.js
+++ base-syntax-example/src/UserInput/UserInput.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const userInput = (props) => {
-    const style 
+    const inputStyle = {
+      border: '2px solid red'
+    };
     return <input 
-    type="text" 
+    type="text"
+    style={inputStyle} 
     onChange={props.changed} 
     value={props.currentName}/>;
 };
