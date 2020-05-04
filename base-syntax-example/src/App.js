5c733a01ffa4adf22c948b0008d9875039367e55
John <jkforsythe@hotmail.com>
2020-05-04 11:00:52 +0100

    userinput props changed

--- base-syntax-example/src/App.js
+++ base-syntax-example/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <UserInput />
+        <UserInput changed={this.usernameChangedHandler}/>
         <UserOutput userName={this.state.username}/>
         <UserOutput userName={this.state.username}/>
         <UserOutput userName="John"/>
